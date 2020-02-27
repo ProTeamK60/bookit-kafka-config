1bc84731a5887eebb8150301d6825bea0fb6a0e3
ola.mattsson <ola.mattsson@gmail.com>
2020-02-27 11:17:48 +0100

    Building up installation

--- setup-instance.sh
+++ setup-instance.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-source install-zookeper.sh $INSTANCE_COUNTER
-source install-kafka.sh $INSTANCE_COUNTER
-source mount-kafka-volume.sh
+source install-zookeper.sh
+#source install-kafka.sh
+#source mount-kafka-volume.sh
