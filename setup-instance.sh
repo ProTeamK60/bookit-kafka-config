03d2c8381995298aed89590a50c0d2688aeced0b
ola.mattsson <ola.mattsson@gmail.com>
2020-02-27 21:20:25 +0100

    Spelling mistake...

--- setup-instance.sh
+++ setup-instance.sh
@@ -1,4 +1,4 @@
 # shellcheck source=/tmp/git/bookit-kafka-config/
-source "$1/install-zookeper.sh"
+source "$1/install-zookeeper.sh"
 #source "$1/install-kafka.sh"
 #source "$1/mount-kafka-volume.sh"
