a06d5698d5ece4331e13f55893033557d3f0a163
ola.mattsson <ola.mattsson@gmail.com>
2020-02-28 16:25:43 +0100

    Invoke mounting of volume from main script

--- setup-instance.sh
+++ setup-instance.sh
@@ -1,5 +1,5 @@
 # shellcheck source=/tmp/git/bookit-kafka-config/
-#source "$1/mount-kafka-volume.sh"
+source "$1/mount-kafka-volume.sh"
 source "$1/install-zookeeper.sh"
 #source "$1/install-kafka.sh"
 source "$1/install-cluster-discovery.sh"
