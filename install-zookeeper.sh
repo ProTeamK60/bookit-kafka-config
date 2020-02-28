4c3fa2f3c2b8ef53cbe95bfcf767c7fc2b749c1c
ola.mattsson <ola.mattsson@gmail.com>
2020-02-28 13:20:20 +0100

    Break out cluster discovery to it's own script

--- install-zookeeper.sh
+++ install-zookeeper.sh
@@ -23,11 +23,4 @@ cp ./packages/zookeeper/service/etc/systemd/system/zookeeper.service /etc/system
 chmod 644 /etc/systemd/system/zookeeper.service
 systemctl daemon-reload
 
-[[ -d ./packages/scripts/opt/bookit/bin && -d ./packages/cron/etc/cron.d/ ]] && mkdir -p /opt/bookit/bin && \
-sed -E 's!¤path.to.cronfile¤!/etc/cron.d/0configure-hosts!;s/¤service.namespace¤/'\
-"${DISCOVERY_SERVICE_NAME}.${NAMESPACE}"'/;s/¤number.of.instances¤/'"${TOTAL_INSTANCES}"'/' \
-./packages/scripts/opt/bookit/bin/configure-hosts.sh > /opt/bookit/bin/configure-hosts.sh
-chmod 744 /opt/bookit/bin/configure-hosts.sh
 
-cp ./packages/cron/etc/cron.d/0configure-hosts /etc/cron.d/0configure-hosts
-chmod 644 /etc/cron.d/0configure-hosts
